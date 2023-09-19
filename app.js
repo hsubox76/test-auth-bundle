bd9a74b53d59d4b8dcae155a433defda0e9d3386
Christina Holland <chholland@google.com>
2023-09-19 13:23:22 -0700

    clean up

--- app.js
+++ app.js
@@ -1,103 +1,23 @@
-import { SDK_VERSION, initializeApp } from "firebase/app";
-import {
-  getAuth,
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
-import { getFunctions } from "firebase/functions";
-import {
-  getFirestore,
-  getDoc,
-  doc,
-  updateDoc,
-  increment,
-} from "firebase/firestore";
-import {
-  getStorage,
-  ref,
-  uploadString,
-  getDownloadURL,
-} from "firebase/storage";
-import { getPerformance } from "firebase/performance";
-import { getRemoteConfig } from "firebase/remote-config";
-import { getAnalytics } from "firebase/analytics";
-import { getMessaging } from "firebase/messaging";
-import { getDatabase } from "firebase/database";
-import { getInstallations, logPlatform } from "@firebase/installations";
-import firebase from "firebase/compat/app";
-import "firebase/compat/analytics";
-import "firebase/compat/auth";
-import "firebase/compat/functions";
-import "firebase/compat/firestore";
-import "firebase/compat/storage";
-import "firebase/compat/performance";
-import "firebase/compat/remote-config";
-import "firebase/compat/messaging";
+import {initializeApp} from "firebase/app";
+import {initializeAuth, GoogleAuthProvider} from 'firebase/auth';
 
-async function main() {
-  console.log("MAIN START");
+//your firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyB3BYpqf_FrZ2WQidSh9Ml04kuXJp3fvVk",
+  authDomain: "chholland-test.firebaseapp.com",
+  databaseURL: "https://chholland-test.firebaseio.com",
+  projectId: "chholland-test",
+  storageBucket: "chholland-test.appspot.com",
+  messagingSenderId: "91336787373",
+  appId: "1:91336787373:web:a3dffe45ec797267",
+  measurementId: "G-RV2DRJVZ88",
+};
 
-  const app = initializeApp({
-    apiKey: "AIzaSyB3BYpqf_FrZ2WQidSh9Ml04kuXJp3fvVk",
-    authDomain: "chholland-test.firebaseapp.com",
-    databaseURL: "https://chholland-test.firebaseio.com",
-    projectId: "chholland-test",
-    storageBucket: "chholland-test.appspot.com",
-    messagingSenderId: "91336787373",
-    appId: "1:91336787373:web:a3dffe45ec797267",
-    measurementId: "G-RV2DRJVZ88",
-  });
-
-  console.log("FIREBASE VERSION", SDK_VERSION);
-
-  console.log("CALLING AUTH");
-  await signInWithEmailAndPassword(
-    getAuth(app),
-    "manualtest@test.com",
-    "abcd1234"
-  );
-
-  onAuthStateChanged(getAuth(app), async (user) => {
-    console.log("user is", user);
-
-    console.log("CALLING FIRESTORE");
-    const ref = doc(getFirestore(app), "testCollection/testDoc");
-    console.log(
-      "testDoc contents:",
-      JSON.stringify((await getDoc(ref)).data())
-    );
-    await updateDoc(ref, { testField: increment(1) });
-    console.log(
-      "testDoc contents after increment:",
-      JSON.stringify((await getDoc(ref)).data())
-    );
-  });
-
-  console.log("INITIALIZING FUNCTIONS");
-  getFunctions(app);
-  console.log("INITIALIZING PERFORMANCE");
-  getPerformance(app);
-  console.log("INITIALIZING REMOTE-CONFIG");
-  getRemoteConfig(app);
-  console.log("INITIALIZING MESSAGING");
-  getMessaging(app);
-  console.log("INITIALIZING INSTALLATIONS");
-  getInstallations(app);
-  console.log("INITIALIZING ANALYTICS");
-  getAnalytics(app);
-  console.log("INITIALIZING DATABASE");
-  getDatabase(app);
-
-  console.log("INITIALIZING STORAGE");
-  const storage = getStorage(app);
-  const reference = ref(storage, "manualtest");
-  const snapshot = await uploadString(reference, "teststring");
-  console.log(snapshot.metadata);
-  const url = await getDownloadURL(reference);
-  const response = await fetch(url);
-  const text = await response.text();
-  console.log("Storage fetched:", text);
-  console.log("MAIN END");
-}
-
-main();
+const app = initializeApp(firebaseConfig);
+// getAuth() includes BrowserPopupRedirectResolver by default, we
+// need to do this to make sure we don't bring it in
+const auth = initializeAuth(app, {popupRedirectResolver: undefined});
+const provider = new GoogleAuthProvider();
+// Console log these to establish that they are not unused code
+console.log(auth);
+console.log(provider);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 var path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = [
   {
-    mode: "development",
+    mode: "production",
     entry: "./app.js",
     output: {
       path: path.resolve(__dirname, "build"),
@@ -36,45 +37,18 @@ module.exports = [
     },
     devtool: "source-map",
     devServer: {
-      contentBase: "./build"
+      static: "./build"
+    },
+    optimization: {
+      // Force it to keep full variable names during minimization
+      // so we can easily debug what got tree-shaken out.
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            mangle: false
+          }
+        })
+      ],
     }
-  },
-  // {
-  //   mode: "development",
-  //   entry: "./firebase.test.js",
-  //   output: {
-  //     path: path.resolve(__dirname, "testbuild"),
-  //     filename: "app.test.js"
-  //   },
-  //   module: {
-  //     rules: [
-  //       {
-  //         test: /\.js$/,
-  //         loader: "babel-loader",
-  //         options: {
-  //           presets: [
-  //             [
-  //               "@babel/preset-env",
-  //               {
-  //                 targets: {
-  //                   node: "10"
-  //                 }
-  //               }
-  //             ]
-  //           ]
-  //         }
-  //       }
-  //     ]
-  //   },
-  //   resolve: {
-  //     mainFields: ["browser", "main"]
-  //   },
-  //   stats: {
-  //     colors: true
-  //   },
-  //   devtool: "source-map",
-  //   devServer: {
-  //     contentBase: "./build"
-  //   }
-  // }
+  }
 ];
